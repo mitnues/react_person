7736eb08afa54eecec043bec8dc5b747d064a3bc
milton <mitnunes.pro.com>
2025-09-24 17:24:54 -0300

    add1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerLabel = '';
+  if (isMarried) {
+    partnerLabel = sex === 'm' ? 'wife' : 'husband';
+  }
+
+  return (
+    <div className='Person'>
+      <div className='Person_name'>{name}</div>
+      {age !== undefined && (
+        <div className='Person_age'> {`Age: ${age}`}</div>
+      )}
+      <div className='Person_partner'>
+        {isMarried 
+        ? `My ${partnerLabel} is ${partnerName}`
+        : `I am not married`}
+      </div>
+    </div>
+  );
+};
