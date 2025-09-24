6b574c7021a7ed0c28e2dc2ec7bb4af1f38bf6b3
milton <mitnunes.pro.com>
2025-09-24 17:40:48 -0300

    add2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const { name, age, sex, isMarried, partner } = person;
 
   let partnerLabel = '';
   if (isMarried) {
-    partnerLabel = sex === 'm' ? 'wife' : 'husband';
+    partnerLabel = sex === 'm'
+     ? 'wife' 
+     : 'husband';
   }
 
   return (
     <div className='Person'>
-      <div className='Person_name'>{name}</div>
+      <div className='Person__name'>{name}</div>
       {age !== undefined && (
-        <div className='Person_age'> {`Age: ${age}`}</div>
+        <div className='Person__age'> {`Age: ${age}`}</div>
       )}
-      <div className='Person_partner'>
+      <div className='Person__partner'>
         {isMarried 
-        ? `My ${partnerLabel} is ${partnerName}`
+        ? `My ${partnerLabel} is ${partner}`
         : `I am not married`}
       </div>
     </div>
